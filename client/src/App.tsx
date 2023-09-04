8bcbc8c70d369f612a198a043462a3a314efe180
vimalsonara <vvsonara95@gmail.com>
2023-09-04 23:01:02 +0530

    WIP TodoItem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import TodoItem from './components/TodoItem';
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="h-screen bg-neutral-900">
+      <TodoItem title={'Sample Title'} description={'Sample Description'} />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -1,5 +1,24 @@
-function TodoItem() {
-  return <div></div>;
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+
+interface TodoItemProps {
+  title: string;
+  description: string;
+}
+
+function TodoItem({ title, description }: TodoItemProps) {
+  return (
+    <div className="max-w-2xl flex justify-between items-center border p-2 rounded">
+      <div>
+        <div className="text-white">{title}</div>
+        <div className="text-white">{description}</div>
+      </div>
+      <div className="flex items-center gap-2">
+        <Checkbox className="bg-white" />
+        <Button variant={'destructive'}>Delete</Button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoItem;
