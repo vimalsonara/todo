03b25e15f0019cc57f15c58360f5ac292365a490
vimalsonara <vvsonara95@gmail.com>
2023-09-16 09:05:36 +0530

    Add User Routes & Controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,44 @@
+import asyncHandler from "express-async-handler";
+
+// @desc    Auth user/set token
+// route    POST api/user/auth
+// @access  public
+const authUser = asyncHandler(async (res, req) => {
+  res.status(200).json({ message: "Auth User" });
+});
+
+// @desc    Register new user
+// route    POST api/users
+// @access  public
+const registerUser = asyncHandler(async (res, req) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc    Logout user
+// route    POST api/users/logout
+// @access  public
+const logoutUser = asyncHandler(async (res, req) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc    Get user profile
+// route    GET api/usersd/profile
+// @access  private
+const getUserProfile = asyncHandler(async (res, req) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// @desc    Update user profile
+// route    PUT api/users/profile
+// @access  private
+const updateUserProfile = asyncHandler(async (res, req) => {
+  res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
