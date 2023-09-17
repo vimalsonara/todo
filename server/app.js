68dbd16381b4e5a4f531d830ef9457ad40d2ef4d
vimalsonara <vvsonara95@gmail.com>
2023-09-17 14:16:20 +0530

    Add Auth & User routes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import Todo from "./models/todo.js";
@@ -19,14 +20,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
 app.use("/", todoRoutes);
 app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is ready"));
 
 // error handling
 app.use(notFound);
 app.use(errorHandler);
-
-app.get("/", (req, res) => res.send("Server is ready"));
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,26 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 // @desc    Auth user/set token
 // route    POST api/user/auth
 // @access  public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc    Register new user
@@ -28,6 +43,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -43,21 +59,51 @@ const registerUser = asyncHandler(async (req, res) => {
 // route    POST api/users/logout
 // @access  public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User Logged Out" });
 });
 
 // @desc    Get user profile
 // route    GET api/usersd/profile
 // @access  private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
 });
 
 // @desc    Update user profile
 // route    PUT api/users/profile
 // @access  private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update user profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
