b4a71703fde95a677843d560299dac4ef5eeda0c
vimalsonara <vvsonara95@gmail.com>
2023-09-22 09:03:56 +0530

    Logout API integration

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,38 @@
+import useUserStore from "../store/UserStore.ts";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Header() {
+  const { logout } = useUserStore();
+
+  const header = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer your-access-token",
+  };
+
+  const handleLogout = async () => {
+    try {
+      const res = await axios.post("/api/users/logout", {
+        headers: header,
+      });
+      if (res) {
+        logout();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="flex justify-between p-5 text-white border-b-2 items-center ">
+    <div className="flex justify-between p-5 text-white border-b-2 items-center">
       <div className="font-bold text-lg">
         <Link to={"/"}>Todo App</Link>
       </div>
       <div>
-        <button className="border p-2 rounded-lg hover:bg-white hover:text-black hover:font-bold">
+        <button
+          className="border p-2 rounded-lg hover:bg-white hover:text-black hover:font-bold"
+          onClick={handleLogout}
+        >
           Logout
         </button>
       </div>
