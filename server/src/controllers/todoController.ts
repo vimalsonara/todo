5f7d2b4cd3fd5a5952d1c4d1b37864c24064dff7
vimalsonara <vvsonara95@gmail.com>
2023-12-17 22:04:42 +0530

    Refactor: Todo controllers  to prisma
    - list all todos
    - create todo

--- server/src/controllers/todoController.ts
+++ server/src/controllers/todoController.ts
@@ -1,41 +1,73 @@
 import asyncHandler from "express-async-handler";
 import Todo from "../models/todo.js";
 import { Request, Response } from "express";
+import db from "../config/db.js";
+import { error } from "console";
 
 // @desc List todos by userId
 // @route POST api/todos
 // @access private
-export const listTodos = (req: Request, res: Response) => {
-  const { userId } = req.body;
+export const listTodos = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.body;
 
-  Todo.find({ userId })
-    .sort({ createdAt: -1 })
-    .then((todos) => {
-      res.status(200).json(todos);
-      console.log("list todos");
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
+    if (!userId) {
+      res.status(400).json("User id can't be empty");
+    }
+
+    const todos = await db.todo.findMany({
+      where: {
+        userId: parseInt(userId),
+      },
     });
-};
+
+    if (todos.length > 0) {
+      res.status(200).json(todos);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // @desc Create new todo
 // route POST api/todos/add
 // @access private
-export const createTodo = (req: Request, res: Response) => {
-  const todo = new Todo(req.body);
+export const createTodo = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const { title, userId, content, isDone } = req.body;
 
-  todo
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
+    if (!userId || !title) {
+      res.status(400).json("Any details can't be empty");
+    }
+
+    const todoData: any = {
+      title,
+      userId,
+    };
+
+    if (content !== undefined) {
+      todoData.content = content;
+    }
+
+    if (isDone !== undefined) {
+      todoData.isDone = isDone;
+    }
+
+    const todo = await db.todo.create({
+      data: todoData,
     });
-};
+
+    if (todo) {
+      res.sendStatus(201);
+    } else {
+      res.status(400).json({ error: "Error while creting todo" });
+    }
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // @desc Update todo status
 // route PUT api/todos/todo
